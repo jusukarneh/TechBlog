78622661c89950ff569349fcc6cf0de1519b9303
jusukarneh <jusudaddi@yahoo.com>
2023-12-12 00:39:28 -0500

    delete comment from profile routes

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
-//http://localhost:3001/profile/admin
+
 router.get("/admin", (req,res)=>{
 
 res.render('',{users:"Jusu",roles:"administrator"})
 })
 
-//http://localhost:3001/profile/user
+
 
 router.get("/admin", (req,res)=>{
 
