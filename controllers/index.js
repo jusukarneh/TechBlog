d21b6f7b0e8ff8ed232491bf7de16af5fb247ff7
jusukarneh <jusudaddi@yahoo.com>
2023-12-12 00:40:48 -0500

    delte cooments in index.js

--- controllers/index.js
+++ controllers/index.js
@@ -5,20 +5,20 @@ const homeRoutes = require('./homeRoutes');
 const profileRoutes = require('./profileRoutes');
 
 router.use('/api', apiRoutes);
-//modular html routes
-//HTML endpoint is meant to present the web pages (VIEWS)
-//http://localhost:3001/
+
+
+
 router.use('/', homeRoutes);
 
- // //http://localhost:3001/profile
+ 
 router.use('/profile', profileRoutes);
 
 
 
 
 
-//API endpoint is meant to do CRUD (MODEL and Controller)
-//http://localhost:3001/api
+
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
